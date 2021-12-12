c1f467ba872cc9e3f84f7cf85d7c88141f1e9db3
Samuel Béland <samuel.beland@gmail.com>
2021-12-12 00:44:45 -0500

    Fixed puzzle descriptions.

--- src/day_1.cpp
+++ src/day_1.cpp
@@ -53,10 +53,6 @@
 //
 // How many measurements are larger than the previous measurement?
 //
-// Your puzzle answer was 1532.
-//
-// The first half of this puzzle is complete! It provides one gold star: *
-//
 //--- Part Two ---
 // Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
 //
